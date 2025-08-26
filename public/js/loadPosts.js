bbed0d9c51caf5d01215f6d1c916f463357929a5
Aidan Habedank <aidan.habedank@iotventures.net>
2025-08-26 14:31:33 +1200

    [HTML] fuck yeah

---
+++ public/js/loadPosts.js
@@ -0,0 +1,3 @@
+export const loadPosts = () => {
+	console.log("Lllllloading!")
+}
---
+++ public/main.js
@@ -0,0 +1,5 @@
+import { loadPosts } from './js/loadPosts.js'
+
+document.addEventListener("DOMContentLoaded", () => {
+	loadPosts();
+})
